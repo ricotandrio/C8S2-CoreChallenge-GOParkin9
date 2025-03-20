8e83d1f02f4ccb2fa8ce75ffe433bdc65cfd93a2
ricotandrio <ricotandrio@gmail.com>
2025-03-20 16:41:22 +0700

    Initial Commit

---
+++ GOParkin9/GOParkin9App.swift
@@ -0,0 +1,17 @@
+//
+//  GOParkin9App.swift
+//  GOParkin9
+//
+//  Created by Rico Tandrio on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GOParkin9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
