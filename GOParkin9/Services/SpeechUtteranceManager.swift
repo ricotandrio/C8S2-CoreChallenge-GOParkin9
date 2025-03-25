39973532321052dcbbf0d5a0efd9d996c72cb3aa
ricotandrio <ricotandrio@gmail.com>
2025-03-25 21:34:58 +0700

    add compass page and add speech utterance feature

---
+++ GOParkin9/Services/SpeechUtteranceManager.swift
@@ -0,0 +1,8 @@
+//
+//  SpeechUtteranceManager.swift
+//  GOParkin9
+//
+//  Created by Rico Tandrio on 25/03/25.
+//
+
+import Foundation
---
+++ GOParkin9/Views/Screens/Home/DetailRecordInactive.swift
@@ -0,0 +1,56 @@
+//
+//  DetailRecordInactive.swift
+//  GOParkin9
+//
+//  Created by Rico Tandrio on 24/03/25.
+//
+
+import SwiftUI
+
+struct DetailRecordInactive: View {
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            
+            Spacer()
+                .frame(height: 100)
+            
+            Image(systemName: "parkingsign.radiowaves.left.and.right.slash")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .foregroundColor(Color.gray)
+            
+            Text("There's no active parking record. Try to park your vehicle.")
+                .padding(.horizontal, 30)
+                .font(.subheadline)
+                .fontWeight(.bold)
+                .opacity(0.6)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+                .frame(height: 80)
+            
+            Button {
+                print("Park Now")
+                
+            } label: {
+                HStack {
+                    Image(systemName: "car")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 15)
+                    
+                    Text("Park Now")
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .foregroundStyle(Color.white)
+                .background(Color.blue)
+                .cornerRadius(8)
+            }
+        }
+    }
+}
