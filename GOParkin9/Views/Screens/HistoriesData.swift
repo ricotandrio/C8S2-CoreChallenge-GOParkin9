eafa29d404bd3c3850beaadc5dae691fe2ff4c55
Chikmah <chikmah519@gmail.com>
2025-03-27 15:10:29 +0700

    history page

--- GOParkin9/Views/Screens/HistoriesData.swift
+++ GOParkin9/Views/Screens/HistoriesData.swift
@@ -5,14 +5,31 @@
 //  Created by Chikmah on 27/03/25.
 //
 
-import SwiftUI
+import Foundation
 
-struct HistoriesData: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct HistoriesData: Identifiable  {
+    var id = UUID()
+    var date: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "d MMMM yyyy" // Example: "28 March 2025"
+        return formatter.string(from: Date())
     }
+    var clockInHour: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm a" // 12-hour format
+        return formatter.string(from: Date())
+    }
+    var clockOutHour: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm a"
+        return formatter.string(from: Date())
+    }
+    
+    var images: [Data]
 }
 
-#Preview {
-    HistoriesData()
-}
+/*
+let testData = [
+    HistoriesData(date: "28 March 2025", clockInHour: "08:40 AM", clockOutHour: "17:03 PM", images: [])
+]
+*/
