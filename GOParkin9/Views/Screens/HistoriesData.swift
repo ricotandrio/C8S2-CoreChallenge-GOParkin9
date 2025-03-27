13e50d75d1b48b57aa2445ddf9cf633f6f0e061e
Chikmah <chikmah519@gmail.com>
2025-03-27 15:09:36 +0700

    history screen

---
+++ GOParkin9/Views/Screens/HistoriesData.swift
@@ -0,0 +1,18 @@
+//
+//  HistoriesData.swift
+//  GOParkin9
+//
+//  Created by Chikmah on 27/03/25.
+//
+
+import SwiftUI
+
+struct HistoriesData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoriesData()
+}
