7b9e74ce5963280271a1925557167e10379f09cc
Rico Tandrio <119276763+ricotandrio@users.noreply.github.com>
2025-03-29 15:07:17 +0700

    Fix navigation link not updating compass view (#8)
    * now gitignore will working
    
    * fix condition when navigation link not updating compass view

--- GOParkin9/Views/Components/DetailRecord.swift
+++ GOParkin9/Views/Components/DetailRecord.swift
@@ -11,10 +11,9 @@ import SwiftData
 struct DetailRecord: View {
     @State private var selectedImageIndex = 0
     @State private var isPreviewOpen = false
-    @State var isCompassOpen = false
     
-
- 
+    @Binding var isCompassOpen: Bool
+    
     @Query(filter: #Predicate<ParkingRecord>{p in p.isHistory == false}) var parkingRecords: [ParkingRecord]
     
 
@@ -68,5 +67,5 @@ struct DetailRecord: View {
 }
 
 #Preview {
-    DetailRecord()
+    ContentView()
 }
